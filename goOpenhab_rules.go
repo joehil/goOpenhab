b3aa90ab6069df5b54a481464442e0a800de90bb
Joerg Hillebrand <hillebrand@jhillebrand.de>
2024-03-04 18:43:15 +0100

    Error in rule corrected

--- goOpenhab_rules.go
+++ goOpenhab_rules.go
@@ -159,7 +159,7 @@ func chronoEvents(mInfo Msginfo) {
 			}
 		}
 	}
-	debugLog(5, fmt.Sprintln("cmd: ", cmd))
+	debugLog(5, fmt.Sprint("cmd: ", cmd))
 	battery(cmd)
 
 	// this rule runs at minutes 1 and 6
