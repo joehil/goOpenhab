0da985a5cc2cbdd8db5b306c967e12ea98f15e88
Joerg Hillebrand <hillebrand@jhillebrand.de>
2025-03-06 17:19:53 +0100

    Add refresh script for building and managing goOpenhab service

--- goOpenhab_doorlock.go
+++ goOpenhab_doorlock.go
@@ -33,19 +33,10 @@ func connect2Doorlock(secrets []int, tags []string, pwds []string) {
 			time.Sleep(time.Second)
 		}
 		for _, element := range pwds {
-			strs := strings.Split(element, ";")
-			if len(strs[0]) == 29 {
-				apwd := strings.ReplaceAll(strs[0], ":", "")
-				decoded, err := hex.DecodeString(apwd)
-				if err != nil {
-					log.Fatal(err)
-				} else {
-					crypted := xcrypt(decoded, secrets)
-					strcrypted := string(crypted)
-					strrnum := string(rNum[:])
-					genVar.Mqttmsg <- Mqttparms{Topic: "doorlock/in/pwd/add", Message: strrnum + strcrypted}
-				}
-			}
+			crypted := xcrypt([]byte(element), secrets)
+			strcrypted := string(crypted)
+			strrnum := string(rNum[:])
+			genVar.Mqttmsg <- Mqttparms{Topic: "doorlock/in/pwd/add", Message: strrnum + strcrypted}
 			time.Sleep(time.Second)
 		}
 
