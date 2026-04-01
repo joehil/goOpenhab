09b70c1920b4d1f1d1ba45baec3d6a76e6e0ac1f
Joerg Hillebrand <hillebrand@jhillebrand.de>
2026-04-01 18:53:01 +0200

    Several modifictions

--- goOpenhab_rules.go
+++ goOpenhab_rules.go
@@ -657,7 +657,7 @@ func processRulesInfo(mInfo Msginfo) {
 			return true // keep iterating
 		})
 		log.Printf("Zigbee Summe Leaves: %.2f  Summe Messages: %.2f\n", summe, messages)
-                if messages < float64(400) {
+                if messages < float64(300) {
                         genVar.Telegram <- "Zigbee neu gestartet"
                         restartZigbee()
                 }
